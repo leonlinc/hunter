e9a071f7c70e7fa4b928cd99c7e5c2d2cdb9f471
llin <leon.lin@harmonicinc.com>
2020-01-31 17:34:13 +0800

    init

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"io"
 	"log"
+	"net"
 	"os"
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
@@ -17,14 +20,19 @@ func main() {
 			Flags: []cli.Flag{
 				&cli.StringFlag{
 					Name:  "i",
-					Value: "eth0",
-					Usage: "specifies network interface to use",
+					Value: "",
+					Usage: "specifies the interface address to use",
+				},
+				&cli.Int64Flag{
+					Name:  "b",
+					Value: 0,
+					Usage: "specifies the bitrate of the file",
 				},
 			},
 			Action: func(c *cli.Context) error {
 				args := c.Args().Slice()
 				if len(args) == 3 {
-					send(args[0], args[1], args[2], c.String("i"))
+					send(args[0], args[1], args[2], c.String("i"), c.Int64("b"))
 				} else {
 					cli.ShowCommandHelpAndExit(c, "s", 1)
 				}
@@ -54,6 +62,54 @@ func main() {
 	}
 }
 
-func send(file, ipaddr, port, iface string) {
-	log.Println("send args:", file, ipaddr, port, iface)
+func send(file, ipaddr, port, iface string, bitrate int64) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	raddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(ipaddr, port))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	laddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(iface, ""))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	conn, err := net.DialUDP("udp", laddr, raddr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	defer conn.Close()
+
+	if bitrate == 0 {
+		log.Fatal("Sorry, please specify the bitrate...")
+	}
+
+	log.Println("local addr:", conn.LocalAddr())
+
+	beginning := time.Now()
+	packet := make([]byte, 1316)
+	ticks := time.Tick(100 * time.Microsecond)
+	var elapsed time.Duration
+	var sent, target int64
+	for _ = range ticks {
+		elapsed = time.Since(beginning)
+		target = int64(float64(bitrate) * elapsed.Seconds())
+		for sent < target {
+			n, err := f.Read(packet)
+			if n != 0 {
+				sent += int64(n * 8)
+				conn.Write(packet[:n])
+			}
+			if err != nil {
+				if err == io.EOF {
+					log.Println("source loop")
+					f.Seek(0, 0)
+				} else {
+					log.Fatalln(err)
+				}
+			}
+		}
+	}
 }
